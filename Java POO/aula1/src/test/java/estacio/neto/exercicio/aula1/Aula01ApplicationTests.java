cb0b30eaf41732936e5bfd05654bd8e2519bee6f
Antonio Neto <antoniogfneto92@hotmail.com>
2021-08-18 18:09:00 -0300

    add aula01

---
+++ Java POO/aula1/src/test/java/estacio/neto/exercicio/aula1/Aula01ApplicationTests.java
@@ -0,0 +1,13 @@
+package estacio.neto.exercicio.aula1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aula01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
