c08735460f256e8279da6855b5c40e7e850adbc0
Antonio Neto <antoniogfneto92@hotmail.com>
2021-08-25 20:47:31 -0300

    update

--- Java POO/aula02/src/aula02/Aula.java
+++ Java POO/aula02/src/aula02/Aula.java
@@ -1,10 +1,20 @@
 package aula02;
 
+import javax.swing.JOptionPane;
+
 public class Aula {
 
 	public static void main(String[] args) {
 
+		Tecnico tecnico = new Tecnico();
 		
+		tecnico.setMatricula(String.valueOf(JOptionPane.showInputDialog(null, "Digite a matrícula: ", "Informe os dados: ", JOptionPane.INFORMATION_MESSAGE, null, null, null)));
+		tecnico.setNome(String.valueOf(JOptionPane.showInputDialog(null, "Digite o nome: ", "Informe os dados: ", JOptionPane.INFORMATION_MESSAGE, null, null, null)));
+		tecnico.setTitulo(String.valueOf(JOptionPane.showInputDialog(null, "Digite o titulo: ", "Informe os dados: ", JOptionPane.INFORMATION_MESSAGE, null, null, null)));
+		
+		JOptionPane.showMessageDialog(null, "Matrícula: " + tecnico.getMatricula() +
+											"\nNome: " + tecnico.getNome() +
+											"\nTítulo: " + tecnico.getTitulo(), "DADOS DO TÉCNICO", JOptionPane.INFORMATION_MESSAGE, null);
 		
 	}
 
---
+++ Java POO/aula02/src/aula02/Empregado.java
@@ -0,0 +1,30 @@
+package aula02;
+
+public class Empregado {
+
+	private String matricula;
+	private String nome;
+	
+	Empregado() {
+		
+	}
+
+	public String getMatricula() {
+		return matricula;
+	}
+
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+	
+}
