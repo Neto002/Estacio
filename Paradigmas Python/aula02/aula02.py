8043c7471989f59eddd8a57441735a2bcf3378fe
Antonio Neto <antoniogfneto92@hotmail.com>
2021-08-26 21:18:53 -0300

    Update aula02.py

--- Paradigmas Python/aula02/aula02.py
+++ Paradigmas Python/aula02/aula02.py
@@ -10,7 +10,7 @@
    c) Solicite tres notas do usuario e calcule a media final'''
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-'''numero1 = int(input('Digite o primeiro número: '))
+numero1 = int(input('Digite o primeiro número: '))
 numero2 = int(input('Digite o segundo número: '))
 
 print(f'A soma entre os dois é {numero1 + numero2}\nSubtração: {numero1 - numero2}\nMultiplicação: {numero1 * numero2}\nResto da divisão: {numero1 % numero2}')
@@ -22,15 +22,17 @@ ano_atual = 2021
 
 nome = str(input('Digite seu nome: ')).title().strip()
 
-print(f'{nome}, você tem {ano_atual - ano_nascimento} anos.')'''
+print(f'{nome}, você tem {ano_atual - ano_nascimento} anos.')
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-nota = [3]
-nota[0] = float(input('Digite sua primeira nota: '))
-for c in range(0, 3):
-    nota[c] = float(input('Digite sua próxima nota: '))
+nota = [float(input('Digite sua primeira nota: ')), float(input('Digite sua próxima nota: ')), float(input('Digite sua próxima nota: '))]
+media = (nota[0] + nota[1] + nota[2]) / 3
 
-print(f'Sua média final é: {(nota[0] + nota[1] + nota[2]) / 3}')
+print(f'Sua média final é: {media:.2f}')
+
+if (media >= 6):
+    print('APROVADO')
+else:
+    print('REPROVADO')
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-
