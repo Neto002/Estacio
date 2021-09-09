6f2dec71c4b170cb43decf6e2080290f3ae4edc2
Antonio Neto <antoniogfneto92@hotmail.com>
2021-09-09 20:49:00 -0300

    implementação em c e c++

--- Paradigmas Python/aula04/aula04.py
+++ Paradigmas Python/aula04/aula04.py
@@ -1,5 +1,6 @@
 #Ler 2 numeros e dizer qual é maior
 
+#Python
 n1 = int(input('Digite o primeiro número: '))
 n2 = int(input('Digite o segundo número: '))
 
@@ -9,3 +10,58 @@ elif (n1 < n2):
     print(f'O maior número entre eles é: {n2}')
 else:
     print('Os números são iguais')
+
+#C
+
+'''
+#include <stdio.h>
+
+int main()
+{
+    int n1, n2 = 0;
+    
+    printf("Digite o primeiro número: ");
+    scanf("%d", &n1);
+    
+    printf("Digite o segundo número: ");
+    scanf("%d", &n2);
+    
+    if (n1 > n2) {
+        printf("O maior número entre eles é %d", n1);
+    } else if (n1 < n2) {
+        printf("O maior número entre eles é %d", n2);
+    } else {
+        printf("Os números são iguais.")
+    }
+
+    return 0;
+}
+'''
+
+#C++
+
+'''
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2 = 0;
+    
+    cout << "Digite o primeiro número: ";
+    cin >> n1;
+    
+    cout << "Digite o segundo número: ";
+    cin >> n2;
+    
+    if (n1 > n2) {
+        cout << "O maior número entre eles é " << n1;
+    } else if (n1 < n2) {
+        cout << "O maior número entre eles é " << n2;
+    } else {
+        cout << "Os números são iguais.";
+    }
+
+    return 0;
+}
+'''
