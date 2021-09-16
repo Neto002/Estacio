342732856ef4fb18e9c35f198df09bf8196f622a
Antonio Neto <antoniogfneto92@hotmail.com>
2021-09-16 20:13:19 -0300

    Update aula04.py
    função para cálculo de fatorial

--- Paradigmas Python/aula04/aula04.py
+++ Paradigmas Python/aula04/aula04.py
@@ -8,3 +8,13 @@ imprimir_Tela()
 def soma(a, b):
     return a + b
 print(soma(5, 1))
+
+def calc_fatorial(numero):
+    i = numero
+    fatorial = 1
+    for i in range(numero, 0, -1):
+        fatorial *= i
+    return fatorial
+
+numero = int(input('Digite um número: '))
+print(f'O valor do fatorial de {numero} é {calc_fatorial(numero)}')
