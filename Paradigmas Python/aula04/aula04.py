82ff1892dff3c0dc281095a7ba7f544e3146750c
Antonio Neto <antoniogfneto92@hotmail.com>
2021-09-16 21:18:44 -0300

    Update aula04.py

--- Paradigmas Python/aula04/aula04.py
+++ Paradigmas Python/aula04/aula04.py
@@ -1,4 +1,4 @@
-#Criando Métodos
+'''#Criando Métodos
 def imprimir_Tela():
     print('Testando esse print')
 imprimir_Tela()
@@ -17,4 +17,43 @@ def calc_fatorial(numero):
     return fatorial
 
 numero = int(input('Digite um número: '))
-print(f'O valor do fatorial de {numero} é {calc_fatorial(numero)}')
+print(f'O valor do fatorial de {numero} é {calc_fatorial(numero)}')'''
+
+#Exercicio 1
+#Faça um programa q leia n notas de um aluno que estejam entre 0 e 10 e informe se o aluno está aprovado (>=7), reprovado (<=5) ou recuperação (>= 5,1 e <= 6,9)
+
+def calcula_media():
+    n = 0
+    qtd = 0
+    media = 0
+    soma = 0
+    while True:
+        while True:
+            n = float(input('Digite a nota: '))
+            if (n >= 0 and n <= 10):
+                break
+            else:
+                print('Por favor, digite um valor de 0 a 10.')
+        soma += n
+        qtd += 1
+        resposta = ''
+        resposta = str(input('Deseja continuar? [S/N] ')).strip().upper() [0]
+        if (resposta in 'N'):
+            break
+    media = soma/qtd
+    return media    
+
+n = calcula_media()
+if (n >= 7):
+    print('APROVADO')
+elif (n <= 5):
+    print('REPROVADO')
+else:
+    print('RECUPERAÇÃO')
+print(f'Nota: {n}')
+
+#Media aritmetica de n notas
+
+print(f'A média aritmética é: {calcula_media():.2f}')
+
+#Leia nome e idade e apresente se a pessoa é maior de idade (>=17) e em q ano nasceu
