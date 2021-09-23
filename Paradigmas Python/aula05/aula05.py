2c0151542c34aa491b5c0b1eade15c49fefd76e1
Antonio Neto <antoniogfneto92@hotmail.com>
2021-09-23 20:07:47 -0300

    Update aula05.py

--- Paradigmas Python/aula05/aula05.py
+++ Paradigmas Python/aula05/aula05.py
@@ -1,7 +1,15 @@
-def func():
+'''def func():
     x = 1
     print(x)
     return x
 
 valor_x = func()
 print("O valor de x é: ", valor_x)
+
+def listsum(numList):
+    theSum = 0
+    for i in numList:
+        theSum += i
+    return theSum
+
+print(listsum([2,4,5,6,3,]))'''
