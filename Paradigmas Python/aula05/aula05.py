b5e591a902509fd57f92cf1603c082304be81f39
Antonio Neto <antoniogfneto92@hotmail.com>
2021-09-23 21:16:10 -0300

    Update aula05.py

--- Paradigmas Python/aula05/aula05.py
+++ Paradigmas Python/aula05/aula05.py
@@ -21,12 +21,11 @@ print(nota)'''
 
 # Elabore um programa que imprima uma sequência da tabuada da potência de base 2 e base 3 de uma lista de 0 até 10.
 
-for i in range (0, 11):
-    print(f'2^{str(i).} = {pow(2, i)}', end=', ' if i < 10 else '.')
-print()
-for i in range (0, 11):
-    print(f'3^{i} = {pow(3, i)}', end=', 'if i < 10 else '.')
-print()
+for i in range (0, 22):
+    if (i <= 10):
+        print(f'2^{i} = {pow(2, i)}', end=', ' if i < 10 else '.\n') 
+    else:
+        print(f'3^{i-11} = {pow(3, i-11)}', end=', 'if i < 21 else '.\n')
 
 # Faça um algoritmo em python q leia 3 notas e faça a media, formatada com duas casas decimais
 
@@ -37,9 +36,12 @@ print(f'A média entre as três notas é: {(notas[0] + notas[1] + notas[2])/3:.2
 
 numero = int(input('Digite um numero: '))
 
-print('Soma:')
-for i in range(1, 11):
-    print(f'{numero} + {i} = {numero + i}')
-print('Multiplicação:')
-for i in range(1, 11):
-    print(f'{numero} x {i} = {numero * i}')
+for i in range(1, 21):
+    if (i <= 10):
+        if(i == 1):
+            print('Soma:')
+        print(f'{numero} + {i} = {numero + i}')
+    else:
+        if(i == 11):
+            print('Multiplicação:')
+        print(f'{numero} x {i-10} = {numero * (i-10)}')
