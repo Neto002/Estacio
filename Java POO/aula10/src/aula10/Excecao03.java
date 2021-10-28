4978f7fad4c96c717d4c77844b84843281b199b4
Antonio Neto <antoniogfneto92@hotmail.com>
2021-10-27 21:16:48 -0300

    Update Excecao03.java

--- Java POO/aula10/src/aula10/Excecao03.java
+++ Java POO/aula10/src/aula10/Excecao03.java
@@ -3,8 +3,29 @@ package aula10;
 public class Excecao03 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		try {
+			
+			
+			
+		} catch (Exception e) {
+			
+			
+			
+		}
+		
 	}
 
+	private static void aumentarLetras() throws Exception {
+		try {
+			
+			
+			
+		} catch (NullPointerException e) {
+			
+			throw new Exception(e);
+			
+		}
+	}
+	
 }
