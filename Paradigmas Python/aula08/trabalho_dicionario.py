96c531dc75b8897379e8a6dbb4e942450f83f9ad
Antonio Neto <antoniogfneto92@hotmail.com>
2021-11-04 20:31:26 -0300

    Update trabalho_dicionario.py

--- Paradigmas Python/aula08/trabalho_dicionario.py
+++ Paradigmas Python/aula08/trabalho_dicionario.py
@@ -1,6 +1,6 @@
-# Elabore um script em Python que leia uma String e conte a quantidade de ocorrências de caracteres dessa string
+'''# Elabore um script em Python que leia uma String e conte a quantidade de ocorrências de caracteres dessa string
 
-'''string = str(input('Digite aqui qualquer coisa para a análise: ')).strip()
+string = str(input('Digite aqui qualquer coisa para a análise: ')).strip()
 i = 0
 
 dicionario = {}
@@ -11,11 +11,11 @@ print(f'A string "{string}" tem ', end='')
 c = 0
 for i, j in dicionario.items():
     c += 1
-    print(f'{j} - {i}, ' if c < len(dicionario) else f'e {j} - {i}.', end='')'''
+    print(f'{j} - {i}, ' if c < len(dicionario) else f'e {j} - {i}.', end='')
 
 # Elabore um script em Python que leia um dicionário 1 e apresente um novo dicionário 2 com os tamanhos dos valores do dicionário 1
 
-'''lista = [(int(input('Chave 1: ')), input('Valor 1: ')), (int(input('Chave 2: ')), input('Valor 2: ')), (int(input('Chave 3: ')), input('Valor 3: ')), (int(input('Chave 4: ')), input('Valor 4: ')), (int(input('Chave 5: ')), input('Valor 5: '))]
+lista = [(int(input('Chave 1: ')), input('Valor 1: ')), (int(input('Chave 2: ')), input('Valor 2: ')), (int(input('Chave 3: ')), input('Valor 3: ')), (int(input('Chave 4: ')), input('Valor 4: ')), (int(input('Chave 5: ')), input('Valor 5: '))]
 
 dicionario1 = dict(lista)
 
@@ -24,7 +24,7 @@ dicionario2 = {}
 for i, j in dicionario1.items():
     dicionario2.update({j:len(j)})
 
-print(dicionario2)'''
+print(dicionario2)
 
 # Converter duas listas em elementos de um dicionário
 
@@ -36,4 +36,4 @@ dicionario = {}
 for i in range(3):
     dicionario.update({chaves[i]:valores[i]})
 
-print(dicionario)
+print(dicionario)'''
