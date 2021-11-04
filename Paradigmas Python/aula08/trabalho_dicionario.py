159087ffd2a7a05dfd4cd919a4070ebd9c398073
Antonio Neto <antoniogfneto92@hotmail.com>
2021-11-04 18:39:56 -0300

    Update trabalho_dicionario.py
    Questão 2 trabalho dicionario

--- Paradigmas Python/aula08/trabalho_dicionario.py
+++ Paradigmas Python/aula08/trabalho_dicionario.py
@@ -1,6 +1,6 @@
 # Elabore um script em Python que leia uma String e conte a quantidade de ocorrências de caracteres dessa string
 
-string = str(input('Digite aqui qualquer coisa para a análise: ')).strip()
+'''string = str(input('Digite aqui qualquer coisa para a análise: ')).strip()
 i = 0
 
 dicionario = {}
@@ -11,8 +11,19 @@ print(f'A string "{string}" tem ', end='')
 c = 0
 for i, j in dicionario.items():
     c += 1
-    print(f'{j} - {i}, ' if c < len(dicionario) else f'e {j} - {i}.', end='')
+    print(f'{j} - {i}, ' if c < len(dicionario) else f'e {j} - {i}.', end='')'''
 
 # Elabore um script em Python que leia um dicionário 1 e apresente um novo dicionário 2 com os tamanhos dos valores do dicionário 1
+
+lista = [(int(input('Chave 1: ')), input('Valor 1: ')), (int(input('Chave 2: ')), input('Valor 2: ')), (int(input('Chave 3: ')), input('Valor 3: ')), (int(input('Chave 4: ')), input('Valor 4: ')), (int(input('Chave 5: ')), input('Valor 5: '))]
+
+dicionario1 = dict(lista)
+
+dicionario2 = {}
+
+for i, j in dicionario1.items():
+    dicionario2.update({j:len(j)})
+
+print(dicionario2)
 
 # Converter duas listas em elementos de um dicionário,
