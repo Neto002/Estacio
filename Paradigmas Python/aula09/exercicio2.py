9869a229a9181e5ef0854a2d7406c0773a0c9ba3
Antonio Neto <antoniogfneto92@hotmail.com>
2021-11-04 21:31:40 -0300

    Update exercicio2.py

--- Paradigmas Python/aula09/exercicio2.py
+++ Paradigmas Python/aula09/exercicio2.py
@@ -1,4 +1,4 @@
-class Pessoa(object):
+'''class Pessoa(object):
     def __init__(self, nome, idade, peso):
         self.nome = nome
         self.idade = idade
@@ -11,3 +11,18 @@ pessoa2 = Pessoa("Carlos", 39, 72)
 
 print(pessoa1.nome)
 pessoa1.andar()
+
+def fatorial(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+    if n > 1:
+        return fatorial(n-1) * n
+
+print(fatorial(5))'''
+
+S = [2*x for x in range(101) if x%2==0]
+A = [x**2+x+1 for x in range(100) if x%2==0]
+print(S)
+print(A)
