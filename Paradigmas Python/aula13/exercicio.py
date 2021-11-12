7c523d44b18576928677345cc7c3b0b4bf142e9d
Antonio Neto <antoniogfneto92@hotmail.com>
2021-11-11 21:43:12 -0300

    update
    exercícios de revisão em aula

--- Paradigmas Python/aula13/exercicio.py
+++ Paradigmas Python/aula13/exercicio.py
@@ -1,3 +1,5 @@
+#Instanciação classe Pessoa
+
 class Pessoa(object):
     def __init__(self, nome, idade, peso):
         self.nome = nome
@@ -10,3 +12,39 @@ class Pessoa(object):
 
 pessoa1 = Pessoa("Juliana", 23, 75)
 pessoa2 = Pessoa("Carlos", 39, 72)
+
+#Calculadora
+
+class Calculadora(object):
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def somar(self):
+        return self.a + self.b
+
+    def subtrair(self):
+        return self.a - self.b
+
+    def multiplicar(self):
+        return self.a * self.b
+    
+    def dividir(self):
+        return self.a / self.b
+
+a = int(input('Digite o valor de a: '))
+b = int(input('Digite o valor de b: '))
+
+calc = Calculadora(a, b)
+print(f'Soma: {calc.somar()}\nSubtração: {calc.subtrair()}\nMultiplicação: {calc.multiplicar()}\nDivisão: {calc.dividir()}')
+
+#Lista
+
+print([x for x in range(5, 101) if x%7==0 and x%5!=0])
+
+#Salario
+
+salario = float(input('Digite seu salário: '))
+porcentagem = 0.15 if salario < 500 else 0.10 if salario >= 500 and salario <= 1000 else 0.05
+
+print(f'Seu novo salário é de R${salario + (salario*porcentagem):.2f}')
