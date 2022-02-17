93a1dfcbc191483d7b520a74a4ee8776707957bc
Antonio Neto <antoniogfneto92@hotmail.com>
2022-02-17 01:49:25 -0300

    update

--- Estrututra de Dados/aula01/main.c
+++ Estrututra de Dados/aula01/main.c
@@ -1,97 +1,98 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+CRT_SECURE_NO_WARNINGS;
+
 int exercicio1() {
     printf("Escreva um algoritmo para ler um valor (do teclado) e escrever (na tela) o seu antecessor\n");
-    short valor;
+    int valor;
 
-	printf("Digite um valor: ");
-	scanf("%d", &valor);
+    printf("Digite um valor: ");
+    scanf_s("%d", &valor);
 
-	short antecessor = valor - 1;
+    int antecessor = valor - 1;
 
-	printf("Antecessor do numero digitado: %d\n", antecessor);
+    printf("Antecessor do numero digitado: %d\n", antecessor);
 }
 
 int exercicio2() {
-    printf("Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.\n");
+    printf("Escreva um algoritmo para ler as dimensoes de um retangulo (base e altura), calcular e escrever a area do retangulo.\n");
     float base;
-	float altura;
-	float areaRetangulo;
+    float altura;
+    float areaRetangulo;
 
-	printf("Digite o valor da base: ");
-	scanf("%f", &base);
+    printf("Digite o valor da base: ");
+    scanf_s("%f", &base);
 
-	printf("Digite o valor da altura: ");
-	scanf("%f", &altura);
+    printf("Digite o valor da altura: ");
+    scanf_s("%f", &altura);
 
-	areaRetangulo = base * altura;
+    areaRetangulo = base * altura;
 
-	printf("A area do retangulo e: %0.2f\n", areaRetangulo);
+    printf("A area do retangulo e: %0.2f\n", areaRetangulo);
 }
 
 int exercicio3() {
-    printf("Faça um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mês com 30 dias\n");
+    printf("Faca um algoritmo que leia a idade de uma pessoa expressa em anos, meses e dias e escreva a idade dessa pessoa expressa apenas em dias. Considerar ano com 365 dias e mes com 30 dias\n");
     int idadeAnos;
-	int idadeMeses;
-	int idadeDias;
+    int idadeMeses;
+    int idadeDias;
 
-	printf("Digite sua idade em anos: ");
-	scanf("%d", &idadeAnos);
+    printf("Digite sua idade em anos: ");
+    scanf_s("%d", &idadeAnos);
 
-	printf("%d anos e quantos meses: ", idadeAnos);
-	scanf("%d", &idadeMeses);
+    printf("%d anos e quantos meses: ", idadeAnos);
+    scanf_s("%d", &idadeMeses);
 
-	printf("%d", idadeAnos);
+    printf("%d anos, %d meses e quantos dias: ", idadeAnos, idadeMeses);
+    scanf_s("%d", &idadeDias);
 
-	printf("%d anos, %d meses e quantos dias: ", idadeAnos, idadeMeses);
-	scanf("%d", &idadeDias);
+    idadeDias = idadeDias + (idadeAnos * 365) + (idadeMeses * 30);
 
-	idadeDias += (idadeAnos*365) + (idadeMeses*30);
-
-	printf("A sua idade somente em dias e: %d\n", idadeDias);
+    printf("A sua idade somente em dias e: %d\n", idadeDias);
 }
 
 int exercicio4() {
-    printf("Escreva um algoritmo para ler o número total de eleitores de um município, o número de votos brancos, nulos e válidos. Calcular e escrever o percentual que cada um representa em relação ao total de eleitores.\n");
-    int totalEleitores;
-	int votoBranco;
-	int votoNulo;
-	int votoValido;
+    printf("Escreva um algoritmo para ler o numero total de eleitores de um municipio, o numero de votos brancos, nulos e validos. Calcular e escrever o percentual que cada um representa em relacao ao total de eleitores.\n");
+    float totalEleitores;
+    float votoBranco;
+    float votoNulo;
+    float votoValido;
 
-	printf("Digite o total de eleitores do municipio: ");
-	scanf("%d", &totalEleitores);
+    printf("Digite o total de eleitores do municipio: ");
+    scanf_s("%d", &totalEleitores);
 
-	printf("Digite o total de votos brancos: ");
-	scanf("%d", &votoBranco);
+    printf("Digite o total de votos brancos: ");
+    scanf_s("%d", &votoBranco);
 
-	printf("Digite o total de votos nulos: ");
-	scanf("%d", &votoNulo);
+    printf("Digite o total de votos nulos: ");
+    scanf_s("%d", &votoNulo);
 
-	printf("Digite o total de votos validos: ");
-	scanf("%d", &votoValido);
+    printf("Digite o total de votos validos: ");
+    scanf_s("%d", &votoValido);
 
-	float porcentoBranco = (totalEleitores / votoBranco)/100;
-	float porcentoNulo = (totalEleitores / votoNulo)/100;
-	float porcentoValido = (totalEleitores / votoValido)/100;
+    float porcentoBranco = (votoBranco/totalEleitores) * 100;
+    float porcentoNulo = (votoNulo / totalEleitores) * 100;
+    float porcentoValido = (votoValido / totalEleitores) * 100;
 
-	printf("Porcentagens: \nVotos Brancos: %.2f%%\nVotos Nulos: %.2f%%\nVotos Validos: %.2f%%\n", porcentoBranco, porcentoNulo, porcentoValido);
+    printf("Porcentagens: \nVotos Brancos: %.2f%%\nVotos Nulos: %.2f%%\nVotos Validos: %.2f%%\n", porcentoBranco, porcentoNulo, porcentoValido);
+}
+
+float calculaSalario(float salario, float reajuste) {
+    float salarioReajustado = salario + (salario * (reajuste / 100));
+    return salarioReajustado;
 }
 
 int exercicio5() {
-    float calculaSalario(float salario, float reajuste) {
-        float salarioReajustado = salario + (salario * (reajuste/100));
-        return salarioReajustado;
-    }
-    printf("// Escreva um algoritmo para ler o salário mensal atual de um funcionário e o percentual de reajuste. Calcular e escrever o valor do novo salário. Faça os cálculos usando função.\n");
+    printf("Escreva um algoritmo para ler o salario mensal atual de um funcionario e o percentual de reajuste. Calcular e escrever o valor do novo salario. Faca os calculos usando função.\n");
     float salario;
     float percentualReajuste;
 
     printf("Digite seu salario mensal: ");
-    scanf("%f", &salario);
+    scanf_s("%f", &salario);
 
     printf("Digite o percentual de reajuste: ");
-    scanf("%f", &percentualReajuste);
+    scanf_s("%f", &percentualReajuste);
 
     float salarioReajustado = calculaSalario(salario, percentualReajuste);
 
@@ -100,90 +101,93 @@ int exercicio5() {
 }
 
 int exercicio6() {
-    printf("Ler um valor e escrever a mensagem “É MAIOR QUE 10!” se o valor lido for maior que 10, caso contrário escrever “NÃO É MAIOR QUE 10!”.\n");
+    printf("Ler um valor e escrever a mensagem \"E MAIOR QUE 10!\" se o valor lido for maior que 10, caso contrario escrever \"NAO E MAIOR QUE 10!\".\n");
     int valor;
 
     printf("Digite um valor: ");
-    scanf("%d", &valor);
+    scanf_s("%d", &valor);
 
     if (valor > 10) {
         printf("E MAIOR QUE 10\n");
-    } else {
+    }
+    else {
         printf("NAO E MAIOR QUE 10\n");
     }
 }
 
 int exercicio7() {
-    printf("Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo).\n");
-    short valor;
+    printf("Ler um valor e escrever se e positivo ou negativo (considere o valor zero como positivo).\n");
+    int valor;
     printf("Digite um valor: ");
-    scanf("%d", &valor);
+    scanf_s("%d", &valor);
 
     if (valor >= 0) {
         printf("Positivo\n");
-    } else {
+    }
+    else {
         printf("Negativo\n");
     }
 }
 
 int exercicio8() {
-    printf("Ler as notas da 1a. e 2a. avaliações de um aluno. Calcular a média aritmética simples e escrever uma mensagem que diga se o aluno foi ou não aprovado (considerar que nota igual ou maior que 6 o aluno é aprovado). Escrever também a média calculada.\n");
+    printf("Ler as notas da 1a. e 2a. avaliacoes de um aluno. Calcular a media aritmetica simples e escrever uma mensagem que diga se o aluno foi ou nao aprovado (considerar que nota igual ou maior que 6 o aluno e aprovado). Escrever também a media calculada.\n");
     float av1, av2, media;
 
     printf("Digite sua primeira nota: ");
-    scanf("%f", &av1);
+    scanf_s("%f", &av1);
 
     printf("Digite sua segunda nota: ");
-    scanf("%f", &av2);
+    scanf_s("%f", &av2);
 
-    media = (av1+av2)/2;
+    media = (av1 + av2) / 2;
 
     printf("Media: %.2f\n", media);
 
     if (media >= 6) {
         printf("APROVADO\n");
-    } else {
+    }
+    else {
         printf("REPROVADO\n");
     }
 }
 
 int exercicio9() {
-    printf("Faça um programa em C que imprima de 1 a 50 na tela. \n");
+    printf("Faca um programa em C que imprima de 1 a 50 na tela. \n");
     for (int i = 1; i <= 50; i++) {
         printf("%d\n", i);
     }
 }
 
 int exercicio10() {
-    printf("Elabore um código que imprima uma sequência de 1 até 100 somente com os números pares dessa sequência.\n");
+    printf("Elabore um codigo que imprima uma sequencia de 1 ate 100 somente com os numeros pares dessa sequencia.\n");
     for (int i = 1; i <= 100; i++) {
-        if (i%2==0) {
-            printf("%d", i);
+        if (i % 2 == 0) {
+            printf("%d\n", i);
         }
     }
 }
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 
-	/*exercicio1();
+    exercicio1();
 
     exercicio2();
 
-	exercicio3();
-
-	exercicio4();
-
-	exercicio5();
-
-	exercicio6();
-
-	exercicio7();
-
-	exercicio8();
-
-	exercicio9();
-
-	exercicio10();*/
     exercicio3();
-	return 0;
+
+    exercicio4();
+
+    exercicio5();
+
+    exercicio6();
+
+    exercicio7();
+
+    exercicio8();
+
+    exercicio9();
+
+    exercicio10();
+
+    return 0;
 }
