79905aaa778506288cebd68b46fd75bf2e69df0d
Antonio Neto <antoniogfneto92@hotmail.com>
2022-02-22 20:02:05 -0300

    Update aula02.py

--- Desenvolvimento Rápido Python/aula02/aula02.py
+++ Desenvolvimento Rápido Python/aula02/aula02.py
@@ -1,8 +1,14 @@
-f = open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula02/texto.txt", 'w')
-f.write("Texto 1")
-f.close()
+# with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula02/texto.txt", 'w') as f:
+#     f.write("Texto 1\nTexto 2")
 
+# with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula02/texto.txt", 'r') as f:
+#     print(f.read())
+#     f.close()
 
-f = open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula02/texto.txt", 'r')
-print(f.read())
-f.close()
+# with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula02/arquivo_jogadoresNFL.txt", 'r') as file:
+#     print(file)
+#     print(file.read())
+
+with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula02/arquivo_teste.txt", 'r') as file:
+    for line in file:
+        print(file.readline())
