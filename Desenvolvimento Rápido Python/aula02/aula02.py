5727733cb17f9b00c56ae3240b81c8777b289a5b
Antonio Neto <antoniogfneto92@hotmail.com>
2022-02-22 20:17:25 -0300

    update

--- Desenvolvimento Rápido Python/aula02/aula02.py
+++ Desenvolvimento Rápido Python/aula02/aula02.py
@@ -9,6 +9,17 @@
 #     print(file)
 #     print(file.read())
 
-with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula02/arquivo_teste.txt", 'r') as file:
-    for line in file:
-        print(file.readline())
+# with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula02/arquivo_teste.txt", 'r') as file:
+#     for i in range(7):
+#         print(file.readline())
+
+with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula02/arquivo_escrever.txt", 'w+') as f:
+    texto = ["palavra 1", "palavra 2", "palavra 3"]
+
+    for line in texto:
+        f.write(line)
+        f.write("\n")
+
+with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula02/arquivo_escrever.txt", 'r+') as f:
+    print(texto)
+
