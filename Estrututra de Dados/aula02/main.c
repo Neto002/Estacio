11f180eb28eb4b89e4d6a38ffce329ec697cc5f9
Antonio Neto <antoniogfneto92@hotmail.com>
2022-02-23 19:30:17 -0300

    Update main.c

---
+++ Estrututra de Dados/aula02/main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+
+
+int main()
+
+{
+
+    int i = 10;
+
+    
+printf("Endereco de i: %p\n", &i); // %p = endereco
+
+    printf("Valor de i: %d\n", i);
+
+
+
+    return 0;
+
+}
