ab0a3623dcf50ed6f89878023ed32f1964fef7a0
Antonio Neto <antoniogfneto92@hotmail.com>
2022-02-23 19:57:03 -0300

    update

---
+++ Estrutura de Dados/aula02/main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void troca(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main()
+{
+
+    // int i = 10;
+    // printf("Endereco de i: %p\n", &i); // %p = endereco
+    // printf("Valor de i: %d\n", i);
+
+    // return 0;
+
+    // int a = 5;
+    // int b = 6;
+    // int c;
+    // int *p;
+    // int *q;
+    
+    // p = &a;
+    // q = &b;
+    // c = *p + *q;
+    // printf("c = %d\n", c);
+
+    // return 0;
+
+    int a = 5;
+    int b = 6;
+
+    troca(&a, &b);
+    printf("a: %d  b: %d\n", a, b);
+}
