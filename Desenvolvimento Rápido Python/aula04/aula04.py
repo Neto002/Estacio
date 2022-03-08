eb10742abf48efdddeb6ce848b32d4642f7162cc
Antonio Neto <antoniogfneto92@hotmail.com>
2022-03-08 19:58:57 -0300

    exercicio 2

--- Desenvolvimento Rápido Python/aula04/aula04.py
+++ Desenvolvimento Rápido Python/aula04/aula04.py
@@ -11,10 +11,17 @@ print(palindromo())'''
 # Entrada: AATCTGCAC
 # Saída: CACGTCTAA
 
-with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula04/dna.txt", "w+") as file:
+'''with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula04/dna.txt", "w+") as file:
     dna = input("Digite a cadeia de DNA para análise: ").strip().upper()
     file.write(f"Cadeia normal: {dna}\n")
     file.write(f"Cadeia invertida: {dna[::-1]}\n")
 
 with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula04/dna.txt", "r+") as file:
-    print(file.read())
+    print(file.read())'''
+
+# Faça um programa que leia um texto de um arquivo txt e conte quantas palavras tem nesse arquivo, sem considerar os espaços.
+
+with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula04/textos.txt", "r+") as file:
+    texto_arquivo = str(file.read())
+    lista = texto_arquivo.split()
+    print(f'O texto contido no arquivo tem {len(lista)}', 'palavras.' if len(lista) > 1 else 'palavra.')
