ebfa1f2b7e6f06259a6a814cafd0ca5737790d03
Antonio Neto <antoniogfneto92@hotmail.com>
2022-03-08 19:53:28 -0300

    exercicio

--- Desenvolvimento Rápido Python/aula04/aula04.py
+++ Desenvolvimento Rápido Python/aula04/aula04.py
@@ -1,8 +1,20 @@
-def palindromo():
+'''def palindromo():
     string = input("Digite a palavra ou frase: ").strip().replace(" ", "").lower()
     if string == string[::-1]:
         return "É palíndromo"
     else:
         return "Não é palíndromo"
 
-print(palindromo())
+print(palindromo())'''
+
+# Elabore um programa em Python que leia uma cadeia de DNA e gera a cadeia inversa. Faça a leitura da cadeia utilizando arquivo txt. Exemplo:
+# Entrada: AATCTGCAC
+# Saída: CACGTCTAA
+
+with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula04/dna.txt", "w+") as file:
+    dna = input("Digite a cadeia de DNA para análise: ").strip().upper()
+    file.write(f"Cadeia normal: {dna}\n")
+    file.write(f"Cadeia invertida: {dna[::-1]}\n")
+
+with open("C:/Users/202102570735/Desktop/Estacio/Desenvolvimento Rápido Python/aula04/dna.txt", "r+") as file:
+    print(file.read())
