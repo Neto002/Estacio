a3a7f79b0109ac226e006c5ad1d1fc8ac792718d
Antonio Neto <88252054+Neto002@users.noreply.github.com>
2022-03-09 19:27:04 -0300

    Create main.c

---
+++ Estrutura de Dados/main.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void exercicio1() {
+    printf("Crie um programa que le 10 valores inteiros, insere em um vetor e mostre na tela os valores lidos.\n");
+    
+    int vetor[10];
+    for (int i = 0; i < 10; i++) {
+        printf("Digite um valor: ");
+        scanf("%d", &vetor[i]);
+    }
+    
+    for (int i = 0; i < 10; i++) {
+        printf("Valor da posição %d: %d\n", i, vetor[i]);
+    }
+}
+
+void exercicio2() {
+    printf("Ler um conjunto de números reais, armazenando-o em vetor e calcular o quadrado das componentes deste vetor,");
+    printf(" armazenando o resultado em outro vetor. Os conjuntos possuem 10 elementos cada. Imprimir todos os conjuntos.\n");
+    
+}
+
+void exercicio3() {
+    
+}
+
+void exercicio4() {
+    
+}
+
+void exercicio5() {
+    
+}
+
+void exercicio6() {
+    
+}
+
+void exercicio7() {
+    
+}
+
+void exercicio8() {
+    
+}
+
+void exercicio9() {
+    
+}
+
+void exercicio10() {
+    
+}
+
+void exercicio11() {
+    
+}
+
+void exercicio12() {
+    
+}
+
+void exercicio13() {
+    
+}
+
+void exercicio14() {
+    
+}
+
+int main()
+{
+    exercicio1();
+    exercicio2();
+    exercicio3();
+    exercicio4();
+    exercicio5();
+    exercicio6();
+    exercicio7();
+    exercicio8();
+    exercicio9();
+    exercicio10();
+    exercicio11();
+    exercicio12();
+    exercicio13();
+    exercicio14();
+
+    return 0;
+}
