063c8a786f2cf1e3c974b4983982f6640a432211
Antonio Neto <88252054+Neto002@users.noreply.github.com>
2022-03-09 20:08:01 -0300

    Update main.c

--- Estrutura de Dados/main.c
+++ Estrutura de Dados/main.c
@@ -4,7 +4,9 @@
 void exercicio1() {
     printf("Crie um programa que le 10 valores inteiros, insere em um vetor e mostre na tela os valores lidos.\n");
     
-    int vetor[10];
+    int *vetor;
+    vetor = (int*)malloc(sizeof(int)*10);
+    
     for (int i = 0; i < 10; i++) {
         printf("Digite um valor: ");
         scanf("%d", &vetor[i]);
@@ -19,7 +21,9 @@ void exercicio2() {
     printf("Ler um conjunto de números reais, armazenando-o em vetor e calcular o quadrado das componentes deste vetor,");
     printf(" armazenando o resultado em outro vetor. Os conjuntos possuem 10 elementos cada. Imprimir todos os conjuntos.\n");
     
-    float vetor[10], vetorQuadrado[10];
+    float *vetor, *vetorQuadrado;
+    vetor = (float*)malloc(sizeof(float)*10);
+    vetorQuadrado = (float*)malloc(sizeof(float)*10);
     
     for (int i = 0; i < 10; i++) {
         printf("Digite um numero: ");
@@ -39,12 +43,51 @@ void exercicio2() {
 }
 
 void exercicio3() {
-    printf("\nLeia um vetor de 10 posições. Contar e escrever quantos valores pares ele possui.\n");
+    printf("\nLeia um vetor de 10 posicoes. Contar e escrever quantos valores pares ele possui.\n");
     
+    int *vetor;
+    vetor = (int*)malloc(sizeof(int)*10);
+    
+    int qtdPar = 0;
+    
+    for (int i = 0; i < 10; i++) {
+        printf("Digite um valor: ");
+        scanf("%d", &vetor[i]);
+        
+        if (vetor[i] % 2 ==0) {
+            qtdPar++;
+        }
+    }
+    
+    printf("\nO vetor tem %d valores pares.\n", qtdPar);
 }
 
 void exercicio4() {
+    printf("Faça um programa que receba do usuário um vetor com 10 posições. Em seguida deverá ser impresso o maior e o menor elemento do vetor.\n");
     
+    int *vetor;
+    vetor = (int*)malloc(sizeof(int)*10);
+    
+    int maior = 0;
+    int menor = 0;
+    
+    for (int i = 0; i < 10; i++) {
+        printf("Digite um valor: ");
+        scanf("%d", &vetor[i]);
+        
+        if (i == 0) {
+            maior = vetor[i];
+            menor = vetor[i];
+        } else {
+            if (vetor[i] > maior) {
+                maior = vetor[i];
+            } else if(vetor[i] < menor) {
+                menor = vetor[i];
+            }
+        }
+    }
+    printf("O maior valor do vetor e: %d\n", maior);
+    printf("O menor valor do vetor e: %d\n", menor);
 }
 
 void exercicio5() {
@@ -91,7 +134,7 @@ int main()
 {
     //exercicio1();
     //exercicio2();
-    exercicio3();
+    //exercicio3();
     exercicio4();
     exercicio5();
     exercicio6();
