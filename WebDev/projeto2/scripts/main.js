6197534a77802d74b7af8bc7f7890b5a234e342f
Antonio Neto <antoniogfneto92@hotmail.com>
2022-03-19 17:29:02 -0300

    update

--- WebDev/projeto2/scripts/main.js
+++ WebDev/projeto2/scripts/main.js
@@ -1,8 +1,11 @@
 function mostraSenha() {
     let senha = document.getElementById("senha")
-    if (senha.type === "password") {
+    let confirmaSenha = document.getElementById("confirmaSenha")
+    if (senha.type === "password" || confirmaSenha.type === "password") {
         senha.type = "text"
+        confirmaSenha.type = "text"
     } else {
         senha.type = "password"
+        confirmaSenha.type = "password"
     }
 }
