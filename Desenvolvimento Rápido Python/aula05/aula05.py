22c92b3b5d9a34b68f0df15f65df772b67fd3566
Antonio Neto <antoniogfneto92@hotmail.com>
2022-03-22 21:18:46 -0300

    Update aula05.py

--- Desenvolvimento Rápido Python/aula05/aula05.py
+++ Desenvolvimento Rápido Python/aula05/aula05.py
@@ -1,4 +1,4 @@
-class Pessoa(object):
+class Pessoa:
     def __init__(self, nome, idade, peso):
         self.nome = nome
         self.idade = idade
@@ -7,7 +7,29 @@ class Pessoa(object):
     def exibe_pessoa(self):
         print(f'Nome: {self.nome}\nIdade: {self.idade}\nPeso: {self.peso}')
 
+class Calculadora:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+    
+    def somar(self):
+        return self.a+self.b
+    
+    def subtrair(self):
+        return self.a-self.b
+    
+    def multiplicar(self):
+        return self.a*self.b
+    
+    def dividir(self):
+        return self.a/self.b
 
-pessoa1 = Pessoa('Neto', 19, 80)
+def main():
+    pessoa1 = Pessoa('Neto', 19, 80)
+    print(pessoa1.exibe_pessoa())
 
-print(pessoa1.exibe_pessoa())
+    calculadora = Calculadora(2, 2)
+    print(calculadora.somar())
+
+if '__name__' == '__main__':
+    main()
