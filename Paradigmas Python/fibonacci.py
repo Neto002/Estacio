4ed637d417058c0d083f5bc27d81563dd4dd82f0
Antonio Neto <antoniogfneto92@hotmail.com>
2022-04-12 01:10:45 -0300

    Update fibonacci.py

--- Paradigmas Python/fibonacci.py
+++ Paradigmas Python/fibonacci.py
@@ -6,7 +6,7 @@ print(termo_1)
 print(termo_2)
 
 for i in range(3,n+1):
-    termo = termo_1 + termo_2
+    termo_atual = termo_1 + termo_2
+    print(termo_atual)
     termo_1 = termo_2
-    termo_2 = termo
-    print(termo)
+    termo_2 = termo_atual
