ffe6528060d23b020b2356ac6d38023c3da2bda7
Antonio Neto <antoniogfneto92@hotmail.com>
2022-04-12 20:41:08 -0300

    Update exercicio.py

--- Desenvolvimento Rápido Python/aula08/exercicio.py
+++ Desenvolvimento Rápido Python/aula08/exercicio.py
@@ -35,6 +35,7 @@ except ValueError as erro:
 # Dado uma data informada separada como entrada (dia, mês e ano) e um horário informado separado (hora, minutos e segundos), imprima a data no formato “dia/mês/ano” e o horário no formato “hora:minutos:segundos” usando o método join. (0,2 pontos)
 
 '''try:
+
     dados = {'dia': input('Digite o dia (dd): '), 'mes': input('Digite o mês (mm): '), 'ano': input('Digite o ano (aaaa): '), 'hora': input('Digite a hora (hh): '), 'minutos': input('Digite os minutos (mm): '), 'segundos': input('Digite os segundos (ss): ')}
 
     if not dados['dia'].isnumeric() or not dados['mes'].isnumeric() or not dados['ano'].isnumeric() or not dados['hora'].isnumeric() or not dados['minutos'].isnumeric() or not dados['segundos'].isnumeric():
@@ -43,15 +44,32 @@ except ValueError as erro:
     for chave in dados.keys():
         if len(dados[chave]) < 2:
             dados[chave] = f'0{dados[chave]}'
+
     data = str.join('/', [dados['dia'], dados['mes'], dados['ano']])
     horario = str.join(':', [dados['hora'], dados['minutos'], dados['segundos']])
 
     print(f'Data: {data}')
     print(f'Horário: {horario}')
+
 except ValueError as erro:
     print(erro)'''
 
 # Uma string é utilizada para representar uma das fitas de uma cadeia de DNA. Para tanto, as bases Adenina, Guanina, Citosina, Timina e Uracila são representadas pelas letras A, G, C, T e U, respectivamente. Deseja-se construir um programa em Python que dada uma sequência de DNA é fornecida a sequência de RNA-m equivalente de acordo com a transformação indicada na Tabela abaixo. (0,2 pontos)
 
-# Uma biblioteca distribui um cartão magnético para que os alunos possam frequentá-la. A senha inicial, enviada  por  email,  é  gerada  automaticamente  a partir  da  data  de  nascimento  do  aluno ('dd/mm/aaaa') do  seguinte  modo:mm'$'dd(invertido)  +  '#'  +  dd'!'mm(invertido)  +  '\'+aaaa. Exemplo: Data  de nascimento:  25/10/1995. Resultado: 25$01#10!52\1995.Escreva  um  programa em  Python que  leia  o dia, mês e ano de nascimento de um aluno e retorne sua senha de acordo com as regras acima. (0,2 pontos)
 
+
+# Uma biblioteca distribui um cartão magnético para que os alunos possam frequentá-la. A senha inicial, enviada  por  email,  é  gerada  automaticamente  a partir  da  data  de  nascimento  do  aluno ('dd/mm/aaaa') do  seguinte  modo: mm'$'dd(invertido)  +  '#'  +  dd'!'mm(invertido)  +  '\'+aaaa. Exemplo: Data  de nascimento:  25/10/1995. Resultado: 25$01#10!52\1995. Escreva  um  programa em  Python que  leia  o dia, mês e ano de nascimento de um aluno e retorne sua senha de acordo com as regras acima. (0,2 pontos)
+
+'''try:
+    dados = {'dia': input('Digite o dia (dd): '), 'mes': input('Digite o mês (mm): '), 'ano': input('Digite o ano (aaaa): ')}
+
+    for chave in dados.keys():
+        if len(dados[chave]) < 2:
+            dados[chave] = f'0{dados[chave]}'
+
+    data = str.join('/', [dados['dia'], dados['mes'], dados['ano']])
+
+    print("A senha é: {}".format(dados['dia'] + "$" + dados['mes'][::-1] + "#" + dados['dia'] + "!" + dados['mes'][::-1] + "\\" + dados['ano']))
+
+except ValueError as e:
+    print(e)'''
