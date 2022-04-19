19a99715364055d1adc7c97fb576f087e5d913dd
Antonio Neto <antoniogfneto92@hotmail.com>
2022-04-19 20:18:30 -0300

    Update exercicio.py

--- Desenvolvimento Rápido Python/aula08/exercicio.py
+++ Desenvolvimento Rápido Python/aula08/exercicio.py
@@ -2,7 +2,7 @@ import os
 
 # Tendo um arquivo de texto .txt, faça um script em Python que leia uma palavra e que verifique se esta palavra está no arquivo e quantas ocorrências dela tem no arquivo. Lembrando que a busca da palavra tem  de  ser  exata,  exemplo: se  a  busca  forpela  palavra “amor” e seu textotiver  a  palavra “amora”,  o programa irá contar, logo, isso deve ser evitado. (0,2pontos)
 
-busca_palavra = input('Digite a palavra a ser buscada: ')
+'''busca_palavra = input('Digite a palavra a ser buscada: ')
 
 try:
     with open(os.path.join(os.getcwd(), 'Desenvolvimento Rápido Python', 'aula08', 'exercicio.txt'), 'r+') as arquivo:
@@ -11,7 +11,7 @@ try:
     qtd_busca = texto_arquivo.count(busca_palavra)
 
     print(f'A palavra amor apareceu {qtd_busca} vezes no arquivo.' if qtd_busca > 1 else f'A palavra amor apareceu {qtd_busca} vez no arquivo.' if qtd_busca == 1 else 'A palavra amor não apareceu no arquivo.')
-    
+
 except FileNotFoundError:
     print('O arquivo não foi encontrado.')
 except Exception as erro:
@@ -55,18 +55,37 @@ try:
     print(f'Horário: {horario}')
 
 except ValueError as erro:
-    print(erro)
+    print(erro)'''
 
 # Uma string é utilizada para representar uma das fitas de uma cadeia de DNA. Para tanto, as bases Adenina, Guanina, Citosina, Timina e Uracila são representadas pelas letras A, G, C, T e U, respectivamente. Deseja-se construir um programa em Python que dada uma sequência de DNA é fornecida a sequência de RNA-m equivalente de acordo com a transformação indicada na Tabela abaixo. (0,2 pontos)
 
-try:
+'''try:
     dna = input('Digite a sequência de DNA: ').upper()
-    if dna not in 'AGCT':
-        raise ValueError('Por favor, insira apenas A, G, C, T ou U.')
-    rna = dna.replace('A', 'U').replace('G', 'C').replace('C', 'G').replace('T', 'A')
+
+    if dna not in 'A':
+        raise ValueError('Por favor, insira apenas bases A, G, C e T.')
+    elif dna not in 'G':
+        raise ValueError('Por favor, insira apenas bases A, G, C e T.')
+    elif dna not in 'C':
+        raise ValueError('Por favor, insira apenas bases A, G, C e T.')
+    elif dna not in 'T':
+        raise ValueError('Por favor, insira apenas bases A, G, C e T.')
+
+    dna = dna.replace('A', 'U').replace('T', 'A')
+    rna = []
+
+    for i in dna:
+        rna.append(i)
+
+    for i, j in enumerate(rna):
+        if j == 'G':
+            rna[i] = 'C'
+        elif j == 'C':
+            rna[i] = 'G'
+
     print(f'A sequência de RNA-m é: {rna}')
 except ValueError as erro:
-    print(erro)
+    print(erro)'''
 
 # Uma biblioteca distribui um cartão magnético para que os alunos possam frequentá-la. A senha inicial, enviada  por  email,  é  gerada  automaticamente  a partir  da  data  de  nascimento  do  aluno ('dd/mm/aaaa') do  seguinte  modo: mm'$'dd(invertido)  +  '#'  +  dd'!'mm(invertido)  +  '\'+aaaa. Exemplo: Data  de nascimento:  25/10/1995. Resultado: 25$01#10!52\1995. Escreva  um  programa em  Python que  leia  o dia, mês e ano de nascimento de um aluno e retorne sua senha de acordo com as regras acima. (0,2 pontos)
 
@@ -79,7 +98,7 @@ try:
 
     data = str.join('/', [dados['dia'], dados['mes'], dados['ano']])
 
-    print("A senha é: {}".format(dados['dia'] + "$" + dados['mes'][::-1] + "#" + dados['dia'] + "!" + dados['mes'][::-1] + "\\" + dados['ano']))
+    print("A senha é: {}".format(dados['mes'] + '$' + dados['dia'][::-1] + '#' + dados['dia'] + '!' + dados['mes'][::-1] + '\\' + dados['ano']))
 
 except ValueError as e:
     print(e)
