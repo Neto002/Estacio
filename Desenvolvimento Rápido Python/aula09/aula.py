dfa404627c3316f1d398fa055d85c98c5766ca1c
Antonio Neto <antoniogfneto92@hotmail.com>
2022-05-17 19:28:22 -0300

    update

--- Desenvolvimento Rápido Python/aula09/aula.py
+++ Desenvolvimento Rápido Python/aula09/aula.py
@@ -6,8 +6,14 @@ tabela = connection.cursor()
 tabela.execute("""CREATE TABLE dados (nome, rg, cpf, data_nasc);""")
 tabela.execute("""INSERT INTO dados VALUES ('João', '123456789', '123456789', '01/01/2000');""")
 
+dados = [('João', '123456789', '123456789', '01/01/2000'), ('João', '123456789', '123456789', '01/01/2000')]
+
+tabela.executemany("INSERT INTO dados VALUES (?, ?, ?, ?);", dados)
+connection.commit
+
+tabela.execute("DELETE FROM dados WHERE nome = 'João'")
 connection.commit()
 
-dados = tuple(tabela.execute("""SELECT * FROM dados;"""))
-
-print(dados)
+print("Lista de todos os registros da tabela:\n")
+for i in tabela.execute("SELECT rowid, * FROM dados ORDER BY nome"):
+    print(i)
