2ab9e3d12494e99f0af0163769af336b2800e711
Antonio Neto <antoniogfneto92@hotmail.com>
2022-05-20 19:53:04 -0300

    update

--- WebDev/aula10/main.js
+++ WebDev/aula10/main.js
@@ -1,3 +1,15 @@
 $.ajax({
+    url: 'http://localhost:8080/WebDev/aula10/script.php',
+    method: 'POST',
+
     
 })
+
+function definicaoObjeto() {
+    this.atributo1 = "testando"
+}
+
+function testaJSON() {
+    let obj = new definicaoObjeto()
+    alert(obj.atributo1)
+}
