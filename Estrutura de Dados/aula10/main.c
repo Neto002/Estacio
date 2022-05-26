452efb587e43d5a8869cdf8ddc9048936cd41b80
Antonio Neto <antoniogfneto92@hotmail.com>
2022-05-25 21:45:38 -0300

    update

---
+++ Estrutura de Dados/aula10/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "operacoesArvore.h"
+
+int main() {
+    ArvB arv;
+    arv.raiz = NULL;
+    No *raiz = NULL;
+
+    return 0;
+}
---
+++ Estrutura de Dados/aula10/operacoesArvore.h
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct no {
+    int item;
+    struct no *esquerda, *direita;
+} No;
+
+typedef struct {
+    No *raiz;
+    int tamanho;
+} ArvB;
+
+No* inserir(No *raiz, int valor) {
+    if (raiz == NULL) {
+        No *novo = (No*) malloc(sizeof(No));
+        novo->item = valor;
+        novo->esquerda = NULL;
+        novo->direita = NULL;
+        return novo;
+    } else {
+        if (valor < raiz->item)
+            raiz->esquerda = inserir(raiz->esquerda, valor);
+        if (valor > raiz->item)
+            raiz->direita = inserir(raiz->direita, valor);
+        return raiz;
+    }
+}
+
+// 1 para valor existente e 0 para valor inexistente na Árvore
+void buscar(No *raiz, int item) {
+    if (raiz == NULL) {
+        printf("%d nao encontrado", item);
+    } else {
+        if (raiz->item == item) {
+            printf("%d encontrado!", item);
+        } else {
+            if (item < raiz->item) {
+                buscar(raiz->esquerda, item);
+            } else if (item > raiz->item) {
+                buscar(raiz->direita, item);
+            }
+        }
+    }
+}
+
+No* remover(No *raiz, int item) {
+    if (raiz == NULL) {
+        printf("Valor nao encontrado!");
+        return NULL;
+    } else {
+        if (raiz->item == item) {
+            if(raiz->esquerda == NULL && raiz->direita == NULL) {
+                free(raiz);
+                return NULL;
+            } else {
+                if (raiz->esquerda == NULL || raiz->direita == NULL) {
+                    No *aux;
+                    if (raiz->esquerda != NULL) {
+                        aux = raiz->esquerda;
+                    } else {
+                        aux = raiz->direita;
+                    }
+                    free(raiz);
+                    return aux;
+                } else {
+                    No *aux = raiz->esquerda;
+                    while (aux->direita != NULL) {
+                        aux = aux->direita;
+                    }
+                    raiz->item = aux->item;
+                    aux->item = item;
+                    raiz->esquerda = remover(raiz->esquerda, item);
+                    return raiz;
+                }
+            }
+        } else {
+            if (item < raiz->item) {
+                raiz->esquerda = remover(raiz->esquerda, item);
+            } else {
+                raiz->direita = remover(raiz->direita, item);
+            }
+            return raiz;
+        }
+    }
+}
+
+void imprimirInOrdem(No *raiz) {
+    if (raiz != NULL) {
+        imprimirInOrdem(raiz->esquerda);
+        printf("%d ", raiz->item);
+        imprimirInOrdem(raiz->direita);
+    }
+}
+
+void imprimirPreOrdem(No *raiz) {
+    if (raiz != NULL) {
+        imprimirPreOrdem(raiz->direita);
+        printf("%d ", raiz->item);
+        imprimirPreOrdem(raiz->esquerda);
+    }
+}
