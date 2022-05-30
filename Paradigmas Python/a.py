7a4ef5402f1e16252a15cc141d2d8402a4399d67
Antonio Neto <antoniogfneto92@hotmail.com>
2022-05-30 11:37:05 -0300

    update

--- Paradigmas Python/a.py
+++ Paradigmas Python/a.py
@@ -1,6 +1,35 @@
-def contaTamanho(numero):
-    tamanhoNumero = len(str(numero))
-    print(f"O número digitado tem {tamanhoNumero} dígitos")
+# Parte 3 Exercício 3
 
-num = int(input("Digite um número: "))
-contaTamanho(num)
+class Calculadora:
+    def __init__(self, A, B):
+        self.a = A
+        self.b = B
+
+    def somar(self):
+        return self.a + self.b
+
+    def subtrair(self):
+        return self.a - self.b
+
+    def multiplicar(self):
+        return self.a * self.b
+
+    def dividir(self):
+        return self.a / self.b
+
+# Programa principal
+
+a = int(input("Digite o valor A: "))
+b = int(input("Digite o valor B: "))
+
+calc = Calculadora(a, b)
+
+somaCalculadora = calc.somar()
+subtracaoCalculadora = calc.subtrair()
+multiplicacaoCalculadora = calc.multiplicar()
+divisaoCalculadora = calc.dividir()
+
+print(f"Soma: {somaCalculadora}")
+print(f"Subtração: {subtracaoCalculadora}")
+print(f"Multiplicação: {multiplicacaoCalculadora}")
+print(f"Divisão: {divisaoCalculadora}")
